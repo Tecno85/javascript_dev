5d4d741c4ac88fcfa10c32ecb0e8ff4d7dbc681c
ivan <tecno85@gmail.com>
2024-08-28 17:53:25 -0500

    Realizando ejercicios Strings

--- Basic/06-strings.js
+++ Basic/06-strings.js
@@ -27,9 +27,13 @@ console.log(greeting.includes("Madrid")); // false
 console.log(greeting.slice(0, 10)); // slice, imprime los elementos del 0 al 9 // Hola, Ivan
 console.log(greeting.replace("Ivan", "Esteban")); // Reemplaza la palabra que le asignemos por una nueva.
 
-// Template Literals (Plantillas Literales)
+// Template Literals (Plantillas Literales) / templates strings (Término informal)
 
 // Son una forma poderosa y flexible de trabajar con candenas de texto. Se utilizan comillas ivertidas (```) en lugar de comillas simples o dobles, lo que permite incluir variables y expresiones directamente en la cadena, sin necesidad de usar el operador de concatenación.
+
+// Características Principales
+
+// 1. Multilínea: Permiten crear cadenas de texto en varias líneas sin necesidad de usar caracteres de escape (\n)
 
 let message = `Hola, este 
 es mi 
@@ -38,7 +42,7 @@ JavaScript`;
 
 console.log(message);
 
-// Interpolación de Variables
+// 2. Interpolación de Variables e inclusión de Expresiones: Permite insertar dinámicamente el valor de variables y evaluar expresiones dentro de una cadena de texto usando la sintaxis `${}`( placeholders o marcadores de posición), haciendo el código más legible y flexible. 
 
-let saludo = "Hola Mundo";
-console.log(saludo);
+let myNombre = "Ismael"
+console.log(`Hola, ${myNombre}!`);
---
+++ Basic/07-strings-excercises.js
@@ -0,0 +1,31 @@
+// 1. Concatena dos cadenas de texto
+
+let firstName = "Ivan";
+let lastName = "Madrid";
+
+console.log("Su nombre es " + firstName + " " + lastName);
+
+// 2. Muestra la longitud de una cadena de texto
+
+console.log(firstName.length);
+
+// 3. Muestra el primer y último carácter de un string
+
+console.log(firstName[0]);
+let ultimoCaracter = firstName[firstName.length - 1];
+console.log(ultimoCaracter);
+
+
+// 4. Convierte a mayúsculas y minúsculas un string
+
+// 5. Crea una cadena de texto en varias líneas
+
+// 6. Interpola el valor de una variable en un string
+
+// 7. Reemplaza todos los espacios en blanco de un string por guiones
+
+// 8. Comprueba si una cadena de texto contiene una palabra concreta
+
+// 9. Comprueba si dos strings son iguales
+
+// 10. Comprueba si dos strings tienen la misma longitud
